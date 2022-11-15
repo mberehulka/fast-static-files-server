e4538a2e4cccc6578df10bba74f5e39e6e2fd70f
murielguedes <murielguedes@proton.me>
2022-11-15 13:04:57 -0300

    Initial commit

--- target/debug/deps/pathdiff-fbc8eb578a66d4af.d
+++
@@ -1,7 +0,0 @@
-C:\Users\muriel\Documents\Projects\gondor-io\target\debug\deps\pathdiff-fbc8eb578a66d4af.rmeta: C:\Users\muriel\.cargo\registry\src\github.com-1ecc6299db9ec823\pathdiff-0.2.1\src\lib.rs
-
-C:\Users\muriel\Documents\Projects\gondor-io\target\debug\deps\libpathdiff-fbc8eb578a66d4af.rlib: C:\Users\muriel\.cargo\registry\src\github.com-1ecc6299db9ec823\pathdiff-0.2.1\src\lib.rs
-
-C:\Users\muriel\Documents\Projects\gondor-io\target\debug\deps\pathdiff-fbc8eb578a66d4af.d: C:\Users\muriel\.cargo\registry\src\github.com-1ecc6299db9ec823\pathdiff-0.2.1\src\lib.rs
-
-C:\Users\muriel\.cargo\registry\src\github.com-1ecc6299db9ec823\pathdiff-0.2.1\src\lib.rs:
