5a122f4c4d2315b172a1e6e31f1c39e1f91b33df
murielguedes <murielguedes@proton.me>
2022-11-17 14:45:50 -0300

    minor fix

--- examples/src/basic.rs
+++ examples/src/basic.rs
@@ -3,6 +3,8 @@ mod tests;
 
 mod static_files;
 
+pub const ADDRESS: &'static str = env!("ADDRESS");
+
 fn on_req(mut req: gondor_io::Request) {
     if req.method().is_get() {
         if let Err(e) = static_files::send_static_file(&mut req) {
@@ -19,10 +21,8 @@ fn on_err(e: std::io::Error) {
 }
 
 fn basic_server() {
-    println!("Server running on http://{}", env!("ADDRESS"));
-    gondor_io::GondorIO::new(env!("ADDRESS"), on_req, on_err)
-        .unwrap()
-        .start();
+    println!("Server running on http://{}", ADDRESS);
+    gondor_io::GondorIO::new(ADDRESS, on_req, on_err).unwrap().start();
 }
 
 fn main() {
