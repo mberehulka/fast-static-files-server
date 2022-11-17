77dbfec36f0c8a011578020a685fbc72b42c875d
murielguedes <murielguedes@proton.me>
2022-11-17 15:20:24 -0300

    typo fix

--- examples/src/tests.rs
+++ examples/src/tests.rs
@@ -11,28 +11,28 @@ fn tests() {
 }
 
 fn ok_test() {
-    let successful_connections = Arc::new(AtomicU64::new(0));
-    let successful_writes = Arc::new(AtomicU64::new(0));
-    let successful_reads = Arc::new(AtomicU64::new(0));
+    let mut successful_connections = 0;
+    let mut successful_writes = 0;
+    let mut successful_reads = 0;
     
     for _ in 0..REQUESTS {
         let conn = std::net::TcpStream::connect(ADDRESS);
         if let Ok(mut conn) = conn {
             if let Ok(_) = conn.write(b"GET /ok HTTP/1.1\r\n\r\ntest") {
-                successful_writes.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                successful_writes += 1;
                 let mut res = String::new();
                 if let Ok(_) = conn.read_to_string(&mut res) {
                     if res == "HTTP/1.1 200 OK\r\nContent-Length: 2\r\nContent-Type: text/plain\r\n\r\nOK" {
-                        successful_reads.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                        successful_reads += 1;
                     }
                 }
             }
-            successful_connections.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            successful_connections += 1;
         }
     }
 
     println!("Requests: {}", REQUESTS);
-    println!("Successful connections: {}", successful_connections.load(std::sync::atomic::Ordering::Relaxed));
-    println!("Successful writes: {}", successful_writes.load(std::sync::atomic::Ordering::Relaxed));
-    println!("Successful reads: {}", successful_reads.load(std::sync::atomic::Ordering::Relaxed));
+    println!("Successful connections: {}", successful_connections);
+    println!("Successful writes: {}", successful_writes);
+    println!("Successful reads: {}", successful_reads);
 }
